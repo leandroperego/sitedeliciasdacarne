92d79787af0aeb68e1ad4d1c66b3a0977d3f4598
Leandro Perego <leperego@hotmail.com>
2022-12-16 18:31:50 -0300

    Ajustes bug menu mobile

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-
+// REFERENCIAS DOM
 let iconMenu = document.querySelector("#menu");
 let telaMenuMobile = document.querySelector("#menuMobile");
+let footerNavMobile = document.querySelector("#menuMobile footer");
+let areaClicavelTelaMenu = document.querySelector("#areaClicavelVazio");
 
 window.onload = function (){
     telaMenuMobile.style.display = "none";
 }
 
-let nav = document.querySelector("nav").cloneNode(true);
-let footerNavMobile = document.querySelector("#menuMobile footer");
+let navDoProjeto = document.querySelector("nav").cloneNode(true);
+div_listaOpcoes.insertBefore(navDoProjeto, footerNavMobile);
 
-div_listaOpcoes.insertBefore(nav, footerNavMobile);
-
-menu.addEventListener(`click`, event =>{
-    telaMenuMobile.style.display = "flex";
-    telaMenuMobile.style.position = "absolute";
-    telaMenuMobile.style.top = `${event.pageY - 45}px`;
-    document.body.style.overflow = "hidden";
+// EVENTS LISTENER
+menu.addEventListener(`click`, () =>{
+    mostrarMenuMobile();
 
     let botaoFecharMenuMobile = document.querySelector("#close_menuMobile");
-
-    botaoFecharMenuMobile.addEventListener(`click`, () => {
-        telaMenuMobile.style.display = "none";
-        document.body.style.overflow = "visible";
-    });
+    botaoFecharMenuMobile.addEventListener(`click`, fecharMenuMobile);
+    areaClicavelTelaMenu.addEventListener(`click`, fecharMenuMobile);
 });
+
+// FUNCTIONS
+function mostrarMenuMobile(){
+    telaMenuMobile.style.display = "flex";
+    telaMenuMobile.style.position = "fixed";
+    telaMenuMobile.style.top = "0px";
+    document.body.style.overflow = "hidden";
+}
+
+function fecharMenuMobile(){
+    telaMenuMobile.style.display = "none";
+    document.body.style.overflow = "visible";
+}
